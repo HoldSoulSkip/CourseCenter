347058d51b5937a6cddad88b57cd301673d46a75
fentingyu <654296661@qq.com>
2015-01-18 14:13:46 +0800

    我的测试

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -17,6 +17,8 @@ namespace CourseCenter
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+//我的测试
             );
         }
     }
