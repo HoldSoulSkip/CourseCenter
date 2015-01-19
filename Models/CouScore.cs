2ae023fcd02ee2328c9cb82d106c091084467f96
HoldSoulSkip <240138187@qq.com>
2015-01-19 19:53:56 +0800

    修改字段

--- Models/CouScore.cs
+++ Models/CouScore.cs
@@ -12,8 +12,8 @@ namespace CourseCenter.Models
         public int CourseId{get;set;}//这张成绩表关联的课程Id
         public int ModuleTag { get; set; }//表示属于具体的模块
         public string ModuleScore{ get; set; }//每个模块教师给的分数
-        public string ModuleTotalScore{ get; set; }//每个模块最终的成绩
-        public string ModuleObjectiveScore{ get; set; }//每个模块客观成绩 冯老师的想法是：系统算出的成绩加上课后习题的成绩
+        public double ModuleTotalScore{ get; set; }//每个模块最终的成绩
+        public double ModuleObjectiveScore{ get; set; }//每个模块客观成绩 冯老师的想法是：系统算出的成绩加上课后习题的成绩
          public string ModuleComment{ get; set; }//每个模块教师给的评语
     }
 }
