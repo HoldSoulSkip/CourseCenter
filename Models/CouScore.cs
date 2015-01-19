2c82c5ee0582e7c9e23af37d20b03f1b38e1d791
HoldSoulSkip <240138187@qq.com>
2015-01-19 19:35:09 +0800

    增加了教师评语-客观成绩-整体最后的成绩--

--- Models/CouScore.cs
+++ Models/CouScore.cs
@@ -11,6 +11,9 @@ namespace CourseCenter.Models
         public Guid StudentId { get; set; }////这张成绩表关联的学生Id
         public int CourseId{get;set;}//这张成绩表关联的课程Id
         public int ModuleTag { get; set; }//表示属于具体的模块
-        public string ModuleScore{ get; set; }//每个模块完成的成绩
+        public string ModuleScore{ get; set; }//每个模块教师给的分数
+        public string ModuleTotalScore{ get; set; }//每个模块最终的成绩
+        public string ModuleObjectiveScore{ get; set; }//每个模块客观成绩 冯老师的想法是：系统算出的成绩加上课后习题的成绩
+         public string ModuleComment{ get; set; }//每个模块教师给的评语
     }
 }
