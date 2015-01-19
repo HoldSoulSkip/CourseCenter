c3ecbff696c49dfedfdffa931e9eacab63389c04
HoldSoulSkip <240138187@qq.com>
2015-01-19 22:58:05 +0800

    添加缩略图字段

--- Models/StudentWorkImage.cs
+++ Models/StudentWorkImage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +12,6 @@ namespace CourseCenter.Models
         public int ModuleTag { set; get; }//模块标示符代表是某一类型的模块：例如1, 模块一，2,模块二，3,模块三
         public string FilePath { get; set; }
         public Guid StudentId { get; set; }
+        public double WorkImgScore { get; set; }
     }
 }
