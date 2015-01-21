5b867676de86e5beb26c8c93c97298ee220af498
HoldSoulSkip <240138187@qq.com>
2015-01-21 14:49:56 +0800

    修改陈老师的学档

--- Controllers/StudentWorkController.cs
+++ Controllers/StudentWorkController.cs
@@ -1,5 +1,8 @@
+using CourseCenter.Common;
+using CourseCenter.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,28 +11,22 @@ namespace CourseCenter.Controllers
 {
     public class StudentWorkController : Controller
     {
-    
         DBEntities db = new DBEntities();
-        //获取用户(学生)cookie，cookie存储是ID
-        Guid studentId =new Guid( TakeCookie.GetCookie("userId"));
-        //数据库帮助类
         ModelHelpers mHelp = new ModelHelpers();
-
-    
-    
-      /// <summary>
+        Guid studentId = new Guid(TakeCookie.GetCookie("userid"));
+        /// <summary>
         /// 获取学生所有的课程
         /// </summary>
         /// <returns></returns>
         public ActionResult WorksIndex()
         {
-            List<Course> listCourse = db.Course.SqlQuery("select * from Courses cr join Stu_Course sc on cr.Id=sc.CourseId and sc.StudentId=@Id", new SqlParameter("@Id", studentId)).ToList();
+            List<Course> listCourse = db.Course.SqlQuery("select * from Courses cr join Stu_Course sc on cr.Id=sc.CourseId and sc.StudentId=@Id", new SqlParameter("@Id",studentId)).ToList();
             ViewBag.listCourse = listCourse;
             return View();
         }
 
 
-            /// <summary>
+        /// <summary>
         /// 返回学生电子学档
         /// </summary>
         /// <param name="courseid"></param>
@@ -44,31 +41,31 @@ namespace CourseCenter.Controllers
             if (!string.IsNullOrEmpty(moduleTag))
             {
                 mTag = Convert.ToInt32(moduleTag);
-            
+
             }
-                cScore = db.CouScore.Where(cs => cs.CourseId == courseid && cs.ModuleTag == mTag).FirstOrDefault();
+            cScore = db.CouScore.Where(cs => cs.CourseId == courseid && cs.ModuleTag == mTag).FirstOrDefault();
             ViewBag.cScore = cScore;
             //--------------------------------------返回课程某一模块的详细----------------------------------------------------------
             Module module = db.Module.Where(m => m.CourseId == courseid && m.ModuleTag == mTag).FirstOrDefault();
             ViewBag.module = module;
 
             //----------------------------------------------返回学生对某一模块的作业----------------------------------------------------
-         StudentWork  sWork=   db.StudentWork.Where(sw => sw.CourseId == courseid && sw.StudentId == studentId && sw.ModuleTag == mTag).FirstOrDefault();
-         ViewBag.sWork = sWork;
-         //---------------------------------------返回某一模块的考试题目 学生的答案 及评分-------------------------------------
-         if (module != null)
-         {
+            StudentWork sWork = db.StudentWork.Where(sw => sw.CourseId == courseid && sw.StudentId == studentId && sw.ModuleTag == mTag).FirstOrDefault();
+            ViewBag.sWork = sWork;
+            //---------------------------------------返回某一模块的考试题目 学生的答案 及评分-------------------------------------
+            if (module != null)
+            {
 
-             List<Erecord> listErecord = mHelp.SqlQuery<Erecord>("select pq.QTitle,psa.Answer,psa.AnswerScore from PaperStudentAnswer as psa join PaperQuestion as pq on pq.MouduleId=@moduleId  and psa.QuestionId=pq.Id and psa.StudentId=@studentId", new SqlParameter[] { new SqlParameter("@studentId", studentId), new SqlParameter("@moduleId", module.Id) }).ToList();
-             ViewBag.listErecord = listErecord;
-         }
-         //--------------------------------------------------------------------------------------------------------------------
+                List<Erecord> listErecord = mHelp.SqlQuery<Erecord>("select pq.QTitle,psa.Answer,psa.AnswerScore from PaperStudentAnswers as psa join PaperQuestions as pq on psa.QuestionId=pq.Id where psa.MouduleTag=@mTag and StudentId=@studentId", new SqlParameter[] { new SqlParameter("@mTag", mTag), new SqlParameter("@studentId", studentId) }).ToList();
+                ViewBag.listErecord = listErecord;
+            }
+            //--------------------------------------------------------------------------------------------------------------------
             return View();
         }
-        
+
     }
-    
-        /// <summary>
+
+    /// <summary>
     /// 这是一个viewmodel ，整合的时候可以单独放到一个文件夹。
     /// </summary>
     public class Erecord
